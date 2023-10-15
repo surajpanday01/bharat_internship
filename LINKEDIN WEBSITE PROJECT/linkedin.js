b16f01adde17022622e4ea4ecc072a0a0696fc42
SURAJ PANDEY <142084737+surajpanday01@users.noreply.github.com>
2023-10-15 10:13:54 +0100

    first update

---
+++ LINKEDIN WEBSITE PROJECT/linkedin.js
@@ -0,0 +1,25 @@
+let profileMenu = document.getElementById("profileMenu");
+
+function toggleMenu(){
+    profileMenu.classList.toggle("open-menu");
+}
+
+
+let sideActivity = document.getElementById("sidebarActivity");
+let moreLink = document.getElementById("showMoreLink");
+
+function toggleActivity(){
+    sideActivity.classList.toggle("open-activity");
+    
+    if(sideActivity.classList.contains("open-activity")){
+        moreLink.innerHTML = "show less <b> - <b>";
+
+    }
+    else{
+        moreLink.innerHTML = "show more <b> + <b>";
+
+    }
+
+
+
+}
---
+++ netflix india/netflixindia.js
@@ -0,0 +1,4 @@
+ alert("this is my NETFLIXINDIA WEB SERVER");
+ prompt("please enter your favourite movie");
+ confirm("do you want to login my NETFLIX WEBPAGE");
+ 
